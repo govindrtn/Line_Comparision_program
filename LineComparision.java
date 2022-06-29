0829736eb1c2623967dea52d18f27a8011a18395
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-29 13:09:47 +0530

    Added: Calculate length of lines are Equal greater or lessthen

--- LineComparision.java
+++ LineComparision.java
@@ -2,10 +2,10 @@ package bridgelabz_assignment;
 
 public class LineComparision {
 	public static void main(String[] args) {
-		int x1 = 4, x2 = 8;
+		int x1 = 2, x2 = 8;
 		int y1 = 4, y2 = 8;
 		
-		int xAxis1 = 4 , xAxis2 = 8;
+		int xAxis1 = 1 , xAxis2 = 8;
 		int yAxis1 = 4 , yAxis2 = 8;
 		
 		Double lineLength_1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
@@ -16,6 +16,14 @@ public class LineComparision {
 		System.out.println("Second Line Length is "+lineLength_2);
 		
 		System.out.println(lineLength_1.equals(lineLength_2));
+		
+		if (lineLength_1 > lineLength_2) {
+			System.out.println("Linelength1 is less then " +lineLength_1);
+		}else if (lineLength_1 < lineLength_2) {
+			System.out.println("Linelength1 is greater then " +lineLength_2);
+		}else {
+			System.out.println("Both lines " + lineLength_1 + " are equal " + lineLength_2 );
+		}
 		
 	}
 
