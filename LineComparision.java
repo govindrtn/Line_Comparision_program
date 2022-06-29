6aa4e8fcf1254b36bec197e37f93e4d81217bb35
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-29 12:35:02 +0530

    Added: Calculate_length of two lines

--- LineComparision.java
+++ LineComparision.java
@@ -1,10 +1,24 @@
 package bridgelabz_assignment;
 
 public class LineComparision {
-	
 	public static void main(String[] args) {
-		System.out.println("welcome to line comparision program");
-	}
+		int x1 = 4, x2 = 8;
+		int y1 = 4, y2 = 8;
+		
+		int xAxis1 = 1 , xAxis2 = 8;
+		int yAxis1 = 4 , yAxis2 = 8;
+		
+		double lineLength_1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		
+		double lineLength_2 = Math.sqrt((xAxis2-xAxis1)*(xAxis2-xAxis1)+(yAxis2-yAxis1)*(yAxis2-yAxis1));
+		
+		System.out.println("First Line Length is "+lineLength_1);
+		System.out.println("Second Line Length is "+lineLength_2);
+		
 	
+		
+		
+ 	
+	}
 
 }
