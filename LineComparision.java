b02f27b81585a6c2390e4e4f753da888e2f239c3
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-29 12:54:52 +0530

    Added: Calculate length of 2 line equal or not

--- LineComparision.java
+++ LineComparision.java
@@ -5,20 +5,18 @@ public class LineComparision {
 		int x1 = 4, x2 = 8;
 		int y1 = 4, y2 = 8;
 		
-		int xAxis1 = 1 , xAxis2 = 8;
+		int xAxis1 = 4 , xAxis2 = 8;
 		int yAxis1 = 4 , yAxis2 = 8;
 		
-		double lineLength_1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		Double lineLength_1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		
-		double lineLength_2 = Math.sqrt((xAxis2-xAxis1)*(xAxis2-xAxis1)+(yAxis2-yAxis1)*(yAxis2-yAxis1));
+		Double lineLength_2 = Math.sqrt((xAxis2-xAxis1)*(xAxis2-xAxis1)+(yAxis2-yAxis1)*(yAxis2-yAxis1));
 		
 		System.out.println("First Line Length is "+lineLength_1);
 		System.out.println("Second Line Length is "+lineLength_2);
 		
-	
+		System.out.println(lineLength_1.equals(lineLength_2));
 		
-		
- 	
 	}
 
 }
